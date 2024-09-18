ee2a1df322645f983a47508517f46cbe71b66605
Senuja2001 <146364291+Senuja2001@users.noreply.github.com>
2024-09-18 13:51:41 +0530

    Start project & create book model with mongoose

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express, { request, response } from "express";
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (request, response) =>{
+    console.log(request);
+    return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+
+    })
+    .catch((error) => {
+        console.log(error);
+    });
